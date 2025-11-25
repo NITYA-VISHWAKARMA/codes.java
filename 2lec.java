10b64505d8ce4a801dda23d91519fec553a12af9
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2025-11-25 21:39:31 +0530

    even&odd

---
+++ 2lec.java
@@ -0,0 +1,12 @@
+// program ro print even and odd number.
+
+class main {
+    public static void main(String[] args) {
+int a=5;
+if(a%2==0){
+    System.out.println("even");
+}else{
+    System.out.println("odd");
+}
+}
+}
