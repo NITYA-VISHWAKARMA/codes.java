5090a45657010c27bbbd3e410f04a0ced501cdbb
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2025-11-25 20:40:42 +0530

    NF

---
+++ Newfile.java
@@ -0,0 +1,39 @@
+// import java.util.ArrayList;
+// class main{
+//     public static void main(String []args){
+//         ArrayList<Integer>list = new ArrayList<>();
+//         list.add(5);
+//         list.add(10);
+//          list.add(1,30);
+//          System.out.println(list);
+//     }
+// }
+import java.util.ArrayList;
+class main{
+    public static void main(String []args){
+        ArrayList<Integer>list = new ArrayList<>();
+        // ArrayList<integer>list1 = new Arraylist<>();
+        list.add(5);
+        list.add(10);
+         list.add(1,30);
+        System.out.println(list);
+        System.out.println(list.addAll(list));
+        // System.out.println(list.get(1));     get
+        // System.out.println(list.set(2,99));  set
+        // System.out.println(list.remove(1));    remove
+        // System.out.println(list.contains(30)); index conatin
+        // System.out.println(list.indexOf(100));
+        // System.out.println(list.lastIndexOf(100));
+        // System.out.println(list.size());
+        // System.out.println(list.isEmpty()); 
+        // for (int i=0; i<list.size();i++){
+        //   System.out.println(list.get(i));normal for loop
+        // for(int num : list){
+        //     System.out.println(num);enhanced for loop
+        }
+        }
+
+
+
+
+// linkedList<Integer> list = new linkedlist<>();
