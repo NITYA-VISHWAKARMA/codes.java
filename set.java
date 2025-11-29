fc5676f73f39d48a53fd735f59ff24e253f2e503
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2025-11-29 19:43:44 +0530

    hashset

---
+++ set.java
@@ -0,0 +1,51 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+// class hello{
+//     public static void main(String []args){
+        // HashSet<Integer> set = new HashSet<>();
+        // int arr[] = {1,2,3,4,10};
+        // int arr1[] = {1,2,3,4};/
+        // set.add(10);
+        // set.add(30);
+        // set.add(20);
+//         for(int x : arr){
+//             for (int y: arr){
+//                 if(arr[x]==arr[y]){
+//                     System.out.println(arr[x]);
+//                 }
+//             }
+//         }
+// //         System.out.println(set);
+//         HashSet<Integer> set1= new HashSet<>();
+//         HashSet<Integer> result = new HashSet<>();
+
+//         for(int x: arr){
+//             set1.add(x);
+//         }
+//         for(int y: arr){
+//             if(set1.contains(y)){
+//                 result.add(y);
+//             }
+//             }
+//             System.out.println(result);
+// }
+//}
+class hello{
+    public static void main(String []args){
+        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+        set.add(10);
+        set.add(20);
+        System.out.println(set);
+    }
+}
+
+
+
+
+
+
+
+
+    
+
