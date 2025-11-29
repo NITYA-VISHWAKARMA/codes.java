0132584e643f84c574a8fe58c763757796c74634
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2025-11-29 19:38:49 +0530

    comparator

---
+++ hello.java
@@ -0,0 +1,79 @@
+// 27/11/2025
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.*;
+// class hello{
+//     public static void main(String []args){
+//         ArrayList<Integer> list = new ArrayList<>();
+//         list.add(1);
+//         list.add(0);
+//         list.add(5);
+//         Collections.sort(list);
+//         System.out.println(list);
+// // import java.util.Comparator;
+
+//         // comparable- do objects ko comparable
+
+//         class student implements Comparable<student>{
+//             int rollN;
+//             String name;
+            
+//                 public student(int rollN,String name ){
+//                     this.rollN=rollN;
+//                      this.name=name;
+//                 }
+//             public int compareTo(student t1){
+//                 return this.rollN-t1.rollN;
+//             }
+//         public String toString(){
+//             return rollN+ " " + name;
+//         }
+//         class hello{
+//             public static void main(String[] args) {
+//                 ArrayList<student> list = new ArrayList<>();
+//                 list.add(new student(10,"naman"));
+//                 list.add(new student(1,"naksh"));
+//                 Collections.sort(list);
+//                 System.out.println(list);
+//             }
+//         }
+//     }
+// }
+// }
+
+    // class student {
+    //         String name;
+    //         int rollN;
+        
+    //             public student(String name,int rollN ){
+    //                  this.name=name;
+    //                   this.rollN=rollN;
+    //             }
+    //         }
+    //             class NameSorted implements Comparator<student>{
+    //                 public int compare(student s1,student s2){
+    //                     return s1.name .compareTo(s2.name);
+    //                 }
+    //             }
+    //             class hello{
+    //                 public static void main(String []args){
+    //                     ArrayList<student> list = new ArrayList<>();
+    //                    list.add(new student("om",1));
+    //                     list.add(new student("nitya",1));
+    //                     Collections.sort(list,new NameSorted());
+    //                     for(student s: list){
+    //                         System.out.println(s.name + " " +s.rollN);
+    //                     } 
+    //                 }
+    //             }    
+    
+
+    
+    
+
+
+
+
+
+
+    
